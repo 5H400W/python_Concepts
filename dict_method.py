b0d1127bd4be26136f75ecc953fb149f6db39ad8
Prashant Dwivedi <ml.er.pd@gmail.com>
2023-02-04 19:27:19 +0530

    adding all files to repo in starting

---
+++ dict_method.py
@@ -0,0 +1,13 @@
+my_dict = {
+    "Fast" : "faster" ,
+    "name" : "prashant",
+    "makes": "food",
+    "place" : "India",
+    "marks":[1,2,5,8,9,7,6,4]
+    
+}
+print(my_dict)
+
+print(my_dict.values())
+
+print(my_dict.keys())
---
+++ testing.py
@@ -0,0 +1,13 @@
+import fractions
+
+
+f1 = input("enter  fruit name : \n")
+f2 = input("enter  fruit name : \n")
+f3 = input("enter  fruit name : \n")
+f4 = input("enter  fruit name : \n")
+f5 = input("enter  fruit name : \n")
+f6 = input("enter  fruit name : \n")
+f7 = input("enter  fruit name : \n")
+
+fruits = [f1,f2,f3,f4,f5,f6,f7]
+print(fruits)
