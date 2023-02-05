c32c90075ae3fea02e24113034b4f67ff75434e8
Prashant Dwivedi <ml.er.pd@gmail.com>
2023-02-05 20:33:19 +0530

    finished recursion and function

--- function_and_recursion.py
+++ function_and_recursion.py
@@ -8,4 +8,22 @@
 Recursion: is function which call itself
 
 """
+#finding factorial using iterative method
+# def facto_iter(n):
+#     product =1
+#     for i in range(n):
+#         product=product*(i+1)
+#     print (product)
 
+# print(facto_iter(5))
+
+#create a program to find factorial of number using ecursion
+
+
+def fact_rec(num): #factoial using recursiv 
+    if num==1 or num==0:
+        return 1
+    return num * fact_rec(num -1)
+
+
+print(fact_rec(0))
